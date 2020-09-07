df7669ebff02067ea5592670a7a0c95f609b047b
muhammadsajidqureshi82 <61787137+muhammadsajidqureshi82@users.noreply.github.com>
2020-09-07 20:48:39 +0500

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,67 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The makeCacheMatrix function take the matrix 'x' and creates an object that
+## can cache inverse of the object x.
+## It enables the user display elements of the matrix and their inverse using the get() and
+## getInverse function.
 
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(x = matrix()) 
+{
+  inv <- NULL
+  
+  set <- function(y) 
+  {
+    x <<- y
+    inv <<- NULL
+  }
+  
+  get <- function() x
+  
+  setInverse <- function(inverse) 
+    inv <<- inverse
+  
+  getInverse <- function() inv
+  list(set = set,
+       get = get,
+       setInverse = setInverse,
+       getInverse = getInverse)
 }
 
 
-## Write a short comment describing this function
+## The cacheSolve function computes the inverse of the matrix returned by makeCacheMatrix function.
+## If the inverse of x is available in the cache,the cachesolve retrieves if from there.
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(x, ...) 
+{
+  inv <- x$getInverse()       
+  
+  if (!is.null(inv)) 
+  {
+    message("Retrieving data from cache")
+    return(inv)     ## Returning  inverse-matrix of 'x'
+  }
+  
+  mat <- x$get()
+  inv <- solve(mat, ...)
+  
+  x$setInverse(inv)
+  
+  inv         ## Returning  inverse-matrix of 'x'
 }
+
+
+##----------------Executing the program----------------------
+
+## making a 3x3 matrix
+my_matrix <- makeCacheMatrix ( matrix(1:9, 3, 3) ) 
+
+## Initializing the 3x3 matrix
+my_matrix$set( matrix( c( 5, 10, 15, 22,25,30, 35, 40, 45 ), 3, 3) )
+
+## Displaying elements in the matrix
+my_matrix$get()
+
+## Displaying inverse of the matrix, before calling the cacheSolve function.
+my_matrix$getInverse()
+
+## Displaying inverse of the matrix, after calling the cacheSolve function.
+cacheSolve(my_matrix)
+my_matrix$getInverse()
